685201743f4aef1b5f3ce095ac3ba3be535b655d
Minsi Yang <yangminsi@gmail.com>
2020-08-20 14:41:24 +0100

    First test added, lower frequency would be replaced by lowFilter, test passed

---
+++ spec/SoundWaveSpec.js
@@ -0,0 +1,10 @@
+describe('Soundwave', function() {
+  it('should replace frequency 10 to frequency 40', function() {
+    let soundwave = new Soundwave()
+    expect(soundwave.bandPassFilter([10], 40, 1000)).toEqual([40])
+  })
+
+  it('should replace frequency [10, 10] to frequency [40, 40]', function() {
+
+  })
+})
---
+++ src/SoundWave.js
@@ -0,0 +1,15 @@
+class Soundwave {
+  // constructor() {
+  //   this.filteredResult = []
+  // }
+  bandPassFilter(frequencies, lowFilter = 40, highFilter) {
+
+    for (let i = 0; i < frequencies.length; i++) {
+      if (frequencies[i] < lowFilter) {
+        frequencies[i] = lowFilter;
+        //  this.filteredResult.push(frequencies[i])
+      }
+      return frequencies
+    }
+  }
+}
